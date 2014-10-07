ef550ded879603581a0eb24bbc492831e4d2cab9
Myla Baker <myla.baker@gmail.com>
2014-10-06 20:13:39 -0700

    Basic HW requirements completed

--- myla_baker/app.js
+++ myla_baker/app.js
@@ -1,1 +1,28 @@
-//Your code here
+var boxes = document.querySelectorAll("td");
+
+var count = 1;
+
+var clickHandler = function() {
+
+        if (count === 1 && this.getAttribute("class") === "empty") {
+            this.innerHTML = "X";
+            count++;
+            this.setAttribute("class", "taken");
+            
+
+            
+        } else if (count === 2 && this.getAttribute("class") === "empty") {
+            this.innerHTML = "O";
+            count = 1;
+            this.setAttribute("class", "taken");
+            
+        }
+ 	}
+ 
+
+for (var i=0; i<boxes.length; i++) {
+	boxes[i].addEventListener("click", clickHandler);
+}
+
+
+
